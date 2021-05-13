ae6a04fcc14b9eae139f772a2b548b52cbab8374
sarathivengadesh <83332004+sarathivengadesh@users.noreply.github.com>
2021-05-13 14:45:13 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import {Route} from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 import './App.scss';
 import Homepage from './pages/Homepage/home.component';
 import Shoppage from './pages/shop/shop.component';
-
+import Header from './component/header/header.component'
 
 function App() {
   return (
     <div className="App">
-     <Route exact path='/' component={Homepage}/>
-     <Route exact path='/shop' component={Shoppage}/>
+      <Header/>
+      <Switch>
+      <Route exact path='/' component={Homepage}/>
+      <Route exact path='/shop' component={Shoppage}/>
+      </Switch>
     </div>
   );
 }
---
+++ src/component/header/header.component.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import {ReactComponent as Logo} from '../../logos/umberla.svg';
+import './header.scss';
+
+const Header = ()=>(
+    <div className='header'>
+        <Link className='Logo-container' to='/'>
+            <Logo className='logo'/>
+        </Link>
+        <div className='options'>
+            <Link className='option' to='/shop'>SHOP</Link>
+            <Link className='option' to='/shop'>CONTACT</Link>
+        </div>
+    </div>
+)
+
+export default Header;
